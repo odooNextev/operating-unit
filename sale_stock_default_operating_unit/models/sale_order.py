794970d16f6d6ae8bd6235bd7232e94af083cfc7
matteo.tognini <matteo.tognini@nextev.it>
2025-07-04 16:54:57 +0200

    [FIX]purchase_operating_unit: fix import e pre commit

--- sale_stock_default_operating_unit/models/sale_order.py
+++ sale_stock_default_operating_unit/models/sale_order.py
@@ -1,12 +1,13 @@
 # © 2025 Nextev
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from odoo import _, api, fields, models
+from odoo import models
+
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     def action_confirm(self):
         context = self.env.context.copy()
-        context['origin_operating_unit'] = self.operating_unit_id.id
+        context["origin_operating_unit"] = self.operating_unit_id.id
         self.env.context = context
         return super().action_confirm()
